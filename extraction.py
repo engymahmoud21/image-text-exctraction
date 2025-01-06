a5d8162086c7cfe2935afdba5de8b09c2fc9f5d7
engymahmoud21 <engy.abuelhassan21@outlook.com>
2025-01-06 13:57:21 +0200

    remove some comments

--- extraction.py
+++ extraction.py
@@ -4,13 +4,7 @@ import json
 import os
 
 def extract_text_from_image(image_path, output_json_path):
-    """
-    Extracts text from an image and saves it to a JSON file.
 
-    Parameters:
-    - image_path: str, path to the input image.
-    - output_json_path: str, path to save the extracted text as a JSON file.
-    """
     # Check if the input image exists
     if not os.path.exists(image_path):
         raise FileNotFoundError(f"The image file '{image_path}' does not exist.")
